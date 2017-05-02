2bd4ffd4a537bcd09aaa58c50c9eb1f2625fb075
edzzh <edgarhart@inbox.lv>
2017-05-02 14:02:26 +0300

    AngularJs Chat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule} from 'angularfire2';
+
+//Must export config
+export const firebaseConfig = {
+  apiKey: "AIzaSyBuCJ5hZdSfOt8RhSvIPJtVFHdUV_6FlpI",
+  authDomain: "angchat-36a91.firebaseapp.com",
+  databaseURL: "https://angchat-36a91.firebaseio.com",
+  storageBucket: "angchat-36a91.appspot.com",
+  messagingSenderId: "1071814737715"
+};
+
+
 
 @NgModule({
   declarations: [
@@ -12,7 +24,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
